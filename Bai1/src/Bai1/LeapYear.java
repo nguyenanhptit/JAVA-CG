15c7fbe10d7d0a9ad189cba93ac6f9b1c0290e76
nguyenanhptit <trannguyenanh96@gmail.com>
2019-03-15 17:26:21 +0700

    first commit

--- Bai1/src/Bai1/LeapYear.java
+++ Bai1/src/Bai1/LeapYear.java
@@ -1,10 +1,31 @@
 package Bai1;
 
+import java.util.Scanner;
+
 public class LeapYear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int year;
+		System.out.println("Enter year: ");
+		year = scanner.nextInt();
+		boolean isLeapYear = false;
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0) {
+					isLeapYear = true;
+				}
+			} else {
+				isLeapYear = true;
 
+			}
+		}
+		if (isLeapYear) {
+			System.out.printf("%d is a leap year ", year);
+		} else {
+			System.out.printf("%d is NOT leap year", year);
+		}
 	}
 
 }
